8f948daca750ce1bdc94a26a0e47e77b43ef04af
Ada <adacaesar2@gmail.com>
2025-05-23 11:30:07 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,8 +1,11 @@
 import random
+# Instead of the difficulty level this code allows the user to select the number of lives they get. I also added a highscore feature and a replay feature.
+highScore = 999
 def thegame():
   print("Hello and welcome to my word guessing game")
   print("You need to guess a number between 1 and 100.")
   lives = int(input("Select the amount of lives you want |"))
+  inL=lives
   thenumber = random.randint(1,100)
   guessedcorrect = False
   res = False
@@ -10,8 +13,13 @@ def thegame():
     guess = int(input("Guess the number. Lives remaining: " + str(lives) + "|"))
     if guess == thenumber:
       print("Congrats :celebrateemoji:")
-      lives = 0
       guessedcorrect = True
+      global highScore
+      if inL-lives+1<highScore:
+        highScore = inL-lives+1
+        print("You beat your PB!")
+      print("Your personal best: " + str(highScore) + " guesses.")  
+      lives = 0
     else:
       if guess > thenumber:
         print("Too big")
