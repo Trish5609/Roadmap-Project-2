7f9f478983545e9040185af92a0c7dc2f94a158b
Ada <adacaesar2@gmail.com>
2025-05-23 11:17:47 +0300

    Update program.py

--- program.py
+++ program.py
@@ -2,20 +2,27 @@ import random
 def thegame():
   print("Hello and welcome to my word guessing game")
   print("You need to guess a number between 1 and 100.")
-  int(input("Select the amount of lives you want") = lives
+  lives = int(input("Select the amount of lives you want |"))
   thenumber = random.randint(1,100)
+  guessedcorrect = False
+  res = False
   while lives > 0:
-    int(input("Guess the number. Lives remaining: " + lives) = guess
+    guess = int(input("Guess the number. Lives remaining: " + str(lives) + "|"))
     if guess == thenumber:
       print("Congrats :celebrateemoji:")
       lives = 0
       guessedcorrect = True
     else:
-      print("Not correct.")
+      if guess > thenumber:
+        print("Too big")
+      if guess < thenumber:
+        print("Too small")  
       lives -= 1
   if guessedcorrect == False:
     print("Oops you ran out of lives")
-  input("Try again? True/False") = Restart
-  if Restart == 1:
+  res = bool(input("Try again? (Leave blank if no)"))
+  if res == True:
     thegame()
-    
+  elif res == False:
+    print("Thank-a you so much-a for playing ma game!")
+thegame()
